6d4affaf8bc8d5d92b38091b6dfe63bd3300e385
chunbikm59 <chunbikm59@gmail.com>
2025-07-28 23:16:19 +0800

    Remove custom OAuth enablement check from provider injection

--- chainlit_app/inject_custom_auth.py
+++ chainlit_app/inject_custom_auth.py
@@ -10,22 +10,6 @@ def random_secret(length: int = 64):
     return "".join((secrets.choice(chars) for i in range(length)))
 
 
-def custom_oauth_enabled():
-    if (
-        os.environ.get("OAUTH_AZURE_AD_B2C_CLIENT_ID") is not None
-        and os.environ.get("OAUTH_AZURE_AD_B2C_CLIENT_SECRET") is not None
-        and os.environ.get("OAUTH_AZURE_AD_B2C_TENANT_ID") is not None
-        and os.environ.get("OAUTH_AZURE_AD_B2C_TENANT_NAME") is not None
-        and os.environ.get("OAUTH_AZURE_AD_B2C_REDIRECT_URL") is not None
-        and os.environ.get("OAUTH_AZURE_AD_B2C_POLICY") is not None
-    ):
-        print("B2C OAuth configured.")
-        return True
-    else:
-        print("B2C OAuth not configured. Skipping...")
-        return False
-
-
 def provider_id_in_instance_list(provider_id: str):
     if providers is None:
         print("No providers found")
@@ -39,7 +23,7 @@ def provider_id_in_instance_list(provider_id: str):
 
 
 def add_custom_oauth_provider(provider_id: str, custom_provider_instance):
-    if custom_oauth_enabled() and not provider_id_in_instance_list(provider_id):
+    if not provider_id_in_instance_list(provider_id):
         providers.append(custom_provider_instance)
         print(f"Added provider: {provider_id}")
     else:
